1cf39df0b526993c48b03500492221c87fc3a592
You-hyeonyeong <dbgusdud412@naver.com>
2019-12-09 16:36:11 +0900

    음식점, 관광지 정보 api 연동

--- routes/testRoute.js
+++ routes/testRoute.js
@@ -3,5 +3,7 @@ var router = express.Router();
 const test = require('../controllers/testController');
 
 router.get('/', test.getTest);
+router.get('/spot', test.touristSpotInfo);
+router.get('/food', test.touristFoodInfo);
 
 module.exports = router;
